4311b4d162d6d3b408844bb4bbbde87832ad2638
Michael Rooplall <MichaelRooplall@gmail.com>
2020-11-06 00:26:45 -0500

    Redesigned login screen

--- at_health/lib/screens/login_screen.dart
+++ at_health/lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:health/screens/home_screen.dart';
@@ -30,113 +31,164 @@ class _LoginScreenState extends State<LoginScreen> {
                         gradient: LinearGradient(
                             begin: Alignment.topRight,
                             end: Alignment.bottomLeft,
-                            colors: [Color(0xffF55E61), Color(0xffFFE4BC)]))),
-                    SingleChildScrollView(
-                      child: Column(
-                          //mainAxisAlignment: MainAxisAlignment.spaceEvenly, TODO
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
-                              child: Container(
-                                height: 200.0,
-                                width: 200.0,
-                                child: Text(
-                                  '@Health',
-                                  style: TextStyle(
-                                      fontSize: 50,
-                                      fontFamily: 'RopaSans',
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.red),
-                                ),
+                            colors: [Color(0xffF55E61), Color(0xffFFE4BC)]
+                        )
+                    )
+                ),
+                Positioned(
+                    bottom: 0,
+                    left : 0,
+                    width : MediaQuery.of(context).size.width,
+                    child: Column(
+                      children : <Widget>[
+                        Image.asset("assets/wave.png",
+
+                          width : MediaQuery.of(context).size.width
+                        ),
+                        Container(
+                          width : MediaQuery.of(context).size.width,
+                          height : 200,
+                          decoration : BoxDecoration(
+                            color : Color(0xffFFE4BC)
+                          )
+                        )
+                      ]
+                    ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    // color : Colors.red
+                  ),
+                  padding: const EdgeInsets.fromLTRB(50, 50, 50, 0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          child: Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              // color : Colors.red
+                            ),
+                            width: double.infinity,
+                            child: Text(
+                              '@Health',
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  fontFamily: 'RopaSans',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red //Color(0xffF55E61)
                               ),
                             ),
-                            Container(
-                              child: Image.asset('assets/health_logo.png'),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          child: Image.asset('assets/health_logo.png'),
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                          child: TextField(
+                            decoration: InputDecoration(hintText: 'AtSign'),
+                            controller: _loginTextFieldController,
+                            onChanged: (value) {
+                              atSign = value;
+                            },
+                          ),
+                        ),
+                        Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              // color : Colors.red
                             ),
-                            TextField(
-                              decoration: InputDecoration(hintText: 'AtSign'),
-                              controller: _loginTextFieldController,
-                              onChanged: (value) {
-                                atSign = value;
-                              },
-                            ),
-                            Container(
-                                width: 500,
-                                height: 200,
-                                child: Row(children: <Widget>[
-                                  Expanded(
-                                      child: SizedBox(
-                                          width: 250,
-                                          child: MaterialButton(
-                                            //padding: EdgeInsets.all(8.0),
-                                            textColor: Colors.white,
-                                            splashColor: Colors.greenAccent,
-                                            elevation: 8.0,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(18.0),
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                              Expanded(
+                                  child: SizedBox(
+                                      width: double.infinity,
+                                      child: MaterialButton(
+                                        //padding: EdgeInsets.all(8.0),
+                                        textColor: Colors.white,
+                                        splashColor: Colors.greenAccent,
+                                        elevation: 8.0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8.0),
+                                        ),
+                                        child: Container(
+                                          width: 145,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(8),
+                                            gradient: LinearGradient(
+                                                begin: Alignment.topRight,
+                                                end: Alignment.bottomLeft,
+                                                colors: [Color(0xff5CCB88), Color(0xff5CCB88)]),
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+                                            child: Text("LOGIN",
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontFamily: 'RopaSans',
+                                                )
                                             ),
-                                            child: Container(
-                                              width: 145,
-                                              decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.circular(18),
-                                                gradient: LinearGradient(
-                                                    begin: Alignment.topRight,
-                                                    end: Alignment.bottomLeft,
-                                                    colors: [Color(0xff5CCB88), Color(0xff5CCB88)]),
-                                              ),
-                                              child: Padding(
-                                                padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-                                                child: Text("LOGIN",
-                                                    textAlign: TextAlign.center,
-                                                    style: TextStyle(
-                                                      fontSize: 20,
-                                                      fontFamily: 'RopaSans',
-                                                    )),
-                                              ),
-                                            ),
-                                            // ),
-                                            onPressed: _login,
-                                          ))
-                                  ),
-                                  Expanded(
-                                      child: SizedBox(
-                                          width: 250,
-                                          child: MaterialButton(
-                                            //padding: EdgeInsets.all(8.0),
-                                            textColor: Colors.white,
-                                            //splashColor: Colors.greenAccent,
-                                            elevation: 8.0,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(18.0),
-                                            ),
-                                            child: Container(
-                                              width: 145,
-                                              decoration: BoxDecoration(
-                                                borderRadius: BorderRadius.circular(18),
-                                                gradient: LinearGradient(
-                                                    begin: Alignment.topRight,
-                                                    end: Alignment.bottomLeft,
-                                                    colors: [Color(0xffF55E61), Color(0xffFF55E61)]),
-                                              ),
-                                              child: Padding(
-                                                padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-                                                child: Text("REGISTER",
-                                                    textAlign: TextAlign.center,
-                                                    style: TextStyle(
-                                                      fontSize: 20,
-                                                      fontFamily: 'RopaSans',
-                                                    )),
-                                              ),
-                                            ),
-                                            // ),
-                                            onPressed: () {
-                                              print('Tapped');
-                                            },
-                                          ))
+                                          ),
+                                        ),
+                                        // ),
+                                        onPressed: _login,
+                                      )
                                   )
-                                ])),
-                          ]),
-                    )]));
+                              ),
+                              Expanded(
+                                  child: SizedBox(
+                                      width: 250,
+                                      child: MaterialButton(
+                                        //padding: EdgeInsets.all(8.0),
+                                        textColor: Colors.white,
+                                        //splashColor: Colors.greenAccent,
+                                        elevation: 8.0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(8.0),
+                                        ),
+                                        child: Container(
+                                          width: 145,
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(8),
+                                            gradient: LinearGradient(
+                                                begin: Alignment.topRight,
+                                                end: Alignment.bottomLeft,
+                                                colors: [Color(0xffF55E61), Color(0xffFF55E61)]
+                                            ),
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+                                            child: Text("REGISTER",
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                  fontSize: 20,
+                                                  fontFamily: 'RopaSans',
+                                                )
+                                            ),
+                                          ),
+                                        ),
+                                        // ),
+                                        onPressed: () {
+                                          print('Tapped');
+                                        },
+                                      )
+                                  )
+                              )
+                            ]
+                            )
+                        ),
+                      ]
+                  ),
+                )
+            ]
+        )
+    );
   }
 
   _login() async {
